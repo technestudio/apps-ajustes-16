e1a549819ba45557fc8d465925c2428c9eaa8973
julieta <thethyhun@hotmail.com>
2023-01-06 17:14:45 +0000

    funcional para odoo 16.0

---
+++ filter_by_brand_and_barcode/models/.ipynb_checkpoints/productos-checkpoint.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, api, fields
+
+class ProductosExtendidos(models.Model):
+    
+    _inherit = 'product.template'
+    _description = 'Extensión a la ficha del Producto'
+    
+    x_studio_marca = fields.Text(string='Marca')
+    
