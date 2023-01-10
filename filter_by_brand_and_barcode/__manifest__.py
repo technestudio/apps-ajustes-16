ec6c2f6a7e0b2f3bab9a8ce3de85d24cb1cf6995
julieta <thethyhun@hotmail.com>
2023-01-10 09:05:41 +0000

    funcional para v15: arreglos listos

--- filter_by_brand_and_barcode/__manifest__.py
+++ filter_by_brand_and_barcode/__manifest__.py
@@ -24,8 +24,7 @@
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Sale',
-    'version': '0.1',    
-    'installed_version': '16.0.0.0',
+    'version': '15.0.0.2',    
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'account', 'sale', 'product'],
