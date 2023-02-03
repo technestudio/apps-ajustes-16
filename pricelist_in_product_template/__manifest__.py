c14c0bc481344afd0a293d4a8dd2786b7c158660
julieta <thethyhun@hotmail.com>
2023-02-03 02:36:48 +0000

    esqueleto inicial

---
+++ pricelist_in_product_template/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Precios extras desde plantilla de producto",
+
+    'summary': """
+        Incluye la lista de precios dentro de la ficha de información del producto.
+    """,
+
+    'description': """
+        Incluye la lista de precios dentro de la ficha de información del producto, permitiendo que se puedan modificar o incluir desde ese nuevo espacio.
+    """,
+
+    'author': "Techne Studio IT & Consulting",
+    'website': "https://technestudioit.com/",
+
+    'category': 'Inventory',
+    'version': '0.1',
+
+    'license': 'Other proprietary',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+    ],
+}
